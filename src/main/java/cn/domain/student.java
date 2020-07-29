082151402247353a49cd659b70f28895a3b0ef37
jake <969128839@qq.com>
2020-07-29 15:23:04 +0800

    Initial commit

---
+++ src/main/java/cn/domain/student.java
@@ -0,0 +1,41 @@
+package cn.domain;
+
+public class student {
+
+    private String name;
+    private int age;
+
+    public student() {
+    }
+
+    public student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    @Override
+    public String toString() {
+        return "student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
